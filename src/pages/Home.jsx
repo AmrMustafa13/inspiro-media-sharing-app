cdc46cbfa31efa7addd9dbaf60347020bc7569e8
Amr Mustafa <amrmostafa1311@gmail.com>
2023-07-06 18:21:36 +0300

    completed sidebar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -20,6 +20,8 @@ const Home = () => {
 
   const { user } = useContext(AuthContext);
 
+  const scrollRef = useRef(null);
+
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -31,6 +33,10 @@ const Home = () => {
     getUserData();
   }, [user]);
 
+  useEffect(() => {
+    scrollRef.current?.scrollTo(0, 0);
+  }, []);
+
   const handleSignOut = async () => {
     await signOut(auth);
     navigate("/login");
@@ -39,33 +45,45 @@ const Home = () => {
   return (
     <div className="flex bg-gray-50 md:flex-row flex-col h-screen transition duration-75 ease-out">
       <div className="hidden md:flex h-screen flex-initial">
-        <Sidebar />
+        <Sidebar setToggleSidebar={setToggleSidebar} userData={userData} />
       </div>
       <div className="flex md:hidden flex-row">
-        <HiMenu
-          fontSize={40}
-          className="cursor-pointer"
-          onClick={() => setToggleSidebar(true)}
-        />
-        <Link to="/">
-          <img src={logo} alt="logo" className="w-28" />
-        </Link>
-        <Link to={`user-profile/${userData?.uid}`}>
-          <img src={userData?.photoURL} alt="logo" className="w-28" />
-        </Link>
-      </div>
-      {toggleSidebar && (
-        <div className="fixed w-4/5 bg-white h-screen overflow-y-auto shadow-md z-10 ">
-          <div className="absolute w-full flex justify-end items-center p-2">
-            <AiFillCloseCircle
-              fontSize={30}
-              onClick={() => setToggleSidebar(false)}
-              className="cursor-pointer"
+        <div className="p-2 w-full flex flex-row justify-between items-center shadow-md">
+          <HiMenu
+            fontSize={40}
+            className="cursor-pointer"
+            onClick={() => setToggleSidebar(true)}
+          />
+          <Link to="/">
+            <img src={logo} alt="logo" className="w-28" />
+          </Link>
+          <Link to={`user-profile/${userData?.uid}`}>
+            <img
+              src={userData?.photoURL}
+              alt="logo"
+              className="w-10 h-10 rounded-full"
             />
-          </div>
-          <Sidebar />
+          </Link>
         </div>
-      )}
+        {toggleSidebar && (
+          <div className="fixed w-4/5 bg-white h-screen overflow-y-auto shadow-md z-10 ">
+            <div className="absolute w-full flex justify-end items-center p-2">
+              <AiFillCloseCircle
+                fontSize={30}
+                onClick={() => setToggleSidebar(false)}
+                className="cursor-pointer"
+              />
+            </div>
+            <Sidebar setToggleSidebar={setToggleSidebar} userData={userData} />
+          </div>
+        )}
+      </div>
+      <div className="pb-2 flex-1 h-screen overflow-y-scroll" ref={scrollRef}>
+        <Routes>
+          <Route path="/user-profile/:userId" element={<UserProfile />} />
+          <Route path="/*" element={<Pins />} />
+        </Routes>
+      </div>
       <button onClick={handleSignOut}>Sign Out</button>
     </div>
   );
